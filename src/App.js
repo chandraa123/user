e3c40b217a01207d8d6b591fb71933a8694a2ea5
chandraa123 <49089389+chandraa123@users.noreply.github.com>
2022-04-18 16:25:21 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./styles.css";
+import faker from "faker";
+export default function App() {
+  return (
+    <div className="ui container comments">
+      <div className="comment">
+        <a href="/" className="avatar">
+          <img alt="avatar" src={faker.image.avatar()} />
+        </a>
+        <div className="content">
+          <a href="/" className="author">
+            sam
+          </a>
+          <div className="metadata">
+            <span className="date"> Today at 6:00PM</span>
+          </div>
+          <div className="text"> Nice Blog post!</div>
+        </div>
+      </div>
+    </div>
+  );
+}
